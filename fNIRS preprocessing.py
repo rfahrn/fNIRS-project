2e5d5f2a5aa739ce2bb43877ebe4f563a19a04db
rfahrn <58358083+rfahrn@users.noreply.github.com>
2022-07-25 09:59:59 +0200

    added files

--- fNIRS preprocessing.py
+++ fNIRS preprocessing.py
@@ -1,5 +1,5 @@
-# author: Rebecka Fahrni
-# fNIRS data anaysis BA project
+# Author: Rebecka Fahrni
+# Project: fNIRS data analysis
 
 import csv
 import mne
@@ -10,25 +10,39 @@ import numpy as np
 import clevercsv
 import re
 
+# Preprocessing of fNIRS
 
-def preprocessing(csv_file_hitachi_path):
-    raw = mne.io.read_raw_hitachi(csv_file_hitachi_path)
+
+def read_hitachi(hitachi_path):
+    """
+    Read Hitachi-csv file
+    :param hitachi_path csv file
+    :returns raw: instance of RawHitachi - raw object containing Hitachi data
+    """
+    raw = mne.io.read_raw_hitachi(hitachi_path)
     return raw
 
-def pos_read(file_path):
+def get_montage(file_path):
+    """
+    Read montage from a file
+    :param file_path: of position: has to be of form '.xyz' or '.csv'
+    :return: montage
+    """
     montage = mne.channels.read_custom_montage(file_path)
     return montage
 
-raw1 = preprocessing('Data/S01/S01_MES_Probe1.csv')
-raw2 = preprocessing('Data/S01/S01_MES_Probe2.csv')
-events = mne.find_events(raw=raw1, initial_event=True)
+# --------------------------------------------------------------------------------------------
+# test on first participant data: S01
 
+raw1 = read_hitachi('Data/S01/S01_MES_Probe1.csv')
+raw2 = read_hitachi('Data/S01/S01_MES_Probe2.csv')
+events = mne.find_events(raw=raw1, initial_event=True)
 sfreq = raw1.info['sfreq']
 report = mne.Report(title='Events example')
 report.add_events(events=events, title='Events from "events"', sfreq=sfreq)
 #report.save('report_events.html', overwrite=True)
 
-raw1.set_montage(pos_read('Data/S01/0001.csv'))
+raw1.set_montage(get_montage('Data/S01/0001.csv'))
 raw1.plot_sensors()
 
 
--- pos_convert.py
+++ pos_convert.py
@@ -1,67 +1,83 @@
-
 import csv
 import pandas as pd
 import os
+
 # output csv:  4 columns containing ch_name, x, y, and z.
 
-with open('Data/S01/0001.pos', 'r') as file:
-    with open('Data/S01/0001.csv', 'w') as out_f:
-        writer = csv.writer(out_f)
-        lines = file.readlines()
-        lines = [line.replace('\n','') for line in lines]
-        header = []
-        indi = []
-        c = []
-        for l in lines:
-            if '[' and ']' in l:
-                header.append(l)
-        for i in header:
-            ind = lines.index(i)
-            indi.append(ind)
-        in_val = list(zip(header, indi))
-        in_val = in_val[10:40]
-        h = ['ch_name', 'x', 'y', 'z']
-        writer.writerow(h)
-        for i in range(len(in_val)):
-            if i != len(in_val)-1:
-                tup = in_val[i]
-                tup2 = in_val[i+1]
-                cont = lines[tup[1]:tup2[1]]
-                ch_n = cont[0]
-                ch_n = ch_n.lstrip('[').rstrip(']')
-                content = cont[1::]
-                x = content[0]
-                x = x.lstrip('X=')
-                y = content[1]
-                y = y.lstrip('Y=')
-                z = content[2]
-                z = z.lstrip('Z=')
-                list_content = [str(ch_n), str(x), str(y), str(z)]
-                writer.writerow(list_content)
-            else:
-                tup = in_val[i]
-                cont = lines[tup[1]::]
-                head = cont[0]
-                content = cont[1::]
-                ch_n = cont[0]
-                ch_n = ch_n.lstrip('[').rstrip(']')
-                x = content[0]
-                x = x.lstrip('X=')
-                y = content[1]
-                y = y.lstrip('Y=')
-                z = content[2]
-                z = z.lstrip('Z=')
-                list_content = [str(ch_n), str(x), str(y), str(z)]
-                writer.writerow(list_content)
+def create_csv(read_filename, outfile):
+    """
+    Creates a .csv file out of .pos data
+    :param read_filename: input file path that gets read
+    :param outfile: path to which the output gets written
+    :return:
+    """
+
+    with open(read_filename, 'r') as file:
+        with open(outfile, 'w') as out_f:
+            writer = csv.writer(out_f)
+            lines = file.readlines()
+            lines = [line.replace('\n', '') for line in lines]
+            header = []
+            indi = []
+            for l in lines:
+                if '[' and ']' in l:
+                    header.append(l)
+            for i in header:
+                ind = lines.index(i)
+                indi.append(ind)
+            in_val = list(zip(header, indi))
+            in_val = in_val[10:40]
+            h = ['ch_name', 'x', 'y', 'z']
+            writer.writerow(h)
+            for i in range(len(in_val)):
+                if i != len(in_val)-1:
+                    tup = in_val[i]
+                    tup2 = in_val[i+1]
+                    cont = lines[tup[1]:tup2[1]]
+                    ch_n = cont[0]
+                    ch_n = ch_n.lstrip('[').rstrip(']')
+                    content = cont[1::]
+                    x = content[0]
+                    x = x.lstrip('X=')
+                    y = content[1]
+                    y = y.lstrip('Y=')
+                    z = content[2]
+                    z = z.lstrip('Z=')
+                    list_content = [str(ch_n), str(x), str(y), str(z)]
+                    writer.writerow(list_content)
+                else:
+                    tup = in_val[i]
+                    cont = lines[tup[1]::]
+                    content = cont[1::]
+                    ch_n = cont[0]
+                    ch_n = ch_n.lstrip('[').rstrip(']')
+                    x = content[0]
+                    x = x.lstrip('X=')
+                    y = content[1]
+                    y = y.lstrip('Y=')
+                    z = content[2]
+                    z = z.lstrip('Z=')
+                    list_content = [str(ch_n), str(x), str(y), str(z)]
+                    writer.writerow(list_content)
+
+    # read .csv of positional .csv file
+    df = pd.read_csv(outfile)
+    df.to_csv(outfile, index=False, sep=',')  # restructure
+    return
+
+
+# test on participant S01
 os.makedirs('C:/Users/rebec/fNIRS-project/Data/S01', exist_ok=True)
+
+# write .pos to .csv
+create_csv('Data/S01/0001.pos', 'Data/S01/0001.csv')
+
+# read .csv file with columns: ch_name,x,y,z and create a dataframe
 df = pd.read_csv('Data/S01/0001.csv')
-df.to_csv('Data/S01/0001.csv', index=False, sep=',')
-df = pd.read_csv('Data/S01/0001.csv')
+print(df)
+
+# segmentation of Probe1 and Probe2 to new .csv files
 probe1 = df[df['ch_name'].str.startswith('Probe1')]
 probe2 = df[df['ch_name'].str.startswith('Probe2')]
 probe1.to_csv('Data/S01/probe1_channel_montage.csv', index=False, sep=',')
 probe2.to_csv('Data/S01/probe2_channel_montage.csv', index=False, sep=',')
-
-
-
-
